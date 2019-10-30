372d164974483bfd121e2c3eb7ad329c15d85824
Joonas <joonas.suurvali@gmail.com>
2019-10-30 21:04:30 +0100

    Update Tabs.js

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -6,9 +6,9 @@ class Tabs extends React.Component {
     render() {
         return (
             <div className="ui secondary menu">
-                <a href="#" value="discMovies" onClick={this.props.tabHandler} className="active item">Movies</a>
-                <a href="#" value="discTV" onClick={this.props.tabHandler} className="item">TV</a>
-                <a href="#" value="general" onClick={this.props.tabHandler} className="item">General</a>
+                <a href="!#" value="discMovies" onClick={this.props.tabHandler} className="active item">Movies</a>
+                <a href="!#" value="discTV" onClick={this.props.tabHandler} className="item">TV</a>
+                <a href="!#" value="general" onClick={this.props.tabHandler} className="item">General</a>
             </div>
         )
     }
